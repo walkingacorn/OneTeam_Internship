25e11075ccccbe5070c9fb098506c4e01c264b5e
walkingacorn <akkusonglover15@gmail.com>
2026-05-04 09:10:59 +0530

    if doesnt always need parentheses

--- CorePython/controlstatement.py
+++ CorePython/controlstatement.py
@@ -19,7 +19,7 @@
 
 #------------------------------------------------------
 x=int(input("Enter a number:"))
-if(x%2==0):
+if x%2==0:
     print(f"{x} is an even number.")
 else:
     print(f"{x} is an odd number.")
