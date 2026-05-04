cbcb903bc3eef1f828c1100d38039e21b6dcd22b
walkingacorn <akkusonglover15@gmail.com>
2026-05-04 10:20:48 +0530

    day 8 if,elif,else statements

---
+++ CorePython/control_statement/controlstatement.py
@@ -0,0 +1,45 @@
+#if_example_1
+
+# x=7
+# if x>0:
+#     print("x is positive")
+
+
+
+#------------------------------------------------------
+#if else_example_2
+
+# x=int(input("Enter a number:"))
+# if(x>0):
+#     print(f"{x} is a postive number")
+# else:
+#     print(f"{x} is negative")
+
+
+
+#------------------------------------------------------
+# x=int(input("Enter a number:"))
+# if x%2==0:
+#     print(f"{x} is an even number.")
+# else:
+#     print(f"{x} is an odd number.")
+    
+#-------------------------------------------------------
+
+
+
+#-------------------------------------------------------
+#Grading system based on scores
+# score=int(input("Enter your score:"))
+# if score>=90 and score<=100:
+#     print("Grade A")
+# elif score>=80 and score<90:
+#     print("Grade B")
+# elif score>=70 and score<80:
+#     print("Grade C")
+# elif score>=60 and score<70:
+#     print("Grade D")
+# elif score>=0 and score<=59:
+#     print("Grade F")
+# else:
+#     print("Invalid score. Please enter a score between 0 and 100.")
---
+++ CorePython/control_statement/task.py
@@ -0,0 +1,54 @@
+#task 1: checking age and citizenship for voting eligibility
+
+# age=int(input("Enter your age:"))
+# if age>=18:
+#     citizen=False
+#     if citizen:
+#         print("You are eligible to vote")
+#     else:
+#         print("You are not eligible since you are not a citizen.")
+# else:
+#     print("You are not eligible because you are below 18.")
+
+#task 2: students score and grade
+
+# score=int(input("Enter your score:"))
+# if score>=95 and score<=100:
+#     print('Congrats you have High Distinction')
+# elif score>=90 and score<95:
+#     print("Grade A")
+# elif score>=80 and score<90:
+#     print("Grade B")
+# elif score>=70 and score<80:
+#     print("Grade C")
+# elif score>=60 and score<70:
+#     print("Grade D")
+# elif score>=0 and score<=59:
+#     print("Grade F")
+# else:
+#     print("Invalid score. Please enter a score between 0 and 100.")
+
+#task 3: positive, negative,zero
+
+# number= int(input("Enter a number:"))
+# if number>=0:
+#     if number==0:
+#         print("It is zero.")
+#     else:
+#         print(f"{number} is a positive number")
+# else:
+#     print(f"{number} is a negative number")  
+
+#task 4: code that checks tempertature and says if it hot, warm, cool or cold
+
+# temp=int(input("Enter the temperature in Celsius:"))
+# if temp<15:
+#     print('It is cold.')
+# elif temp>=15 and temp<25:
+#     print('It is cool.')
+# elif temp>=25 and temp<35:
+#     print('It is warm.')
+# else:
+#     print('It is hot.')
+
+
