1e500ddef39ac7ae81261c5c84293afb8686f997
walkingacorn <akkusonglover15@gmail.com>
2026-05-04 10:28:35 +0530

    largest of three numbers

--- CorePython/control_statement/task.py
+++ CorePython/control_statement/task.py
@@ -51,4 +51,16 @@
 # else:
 #     print('It is hot.')
 
+#largest of three numbers:
+
+# num1=int(input("Enter the first number:"))
+# num2=int(input("Enter the second number:"))
+# num3=int(input("Enter the third number:"))
+
+# if num1>num2 and num1>num3:
+#     print(f"{num1} is largest")
+# elif num2>num1 and num2>num3:
+#     print(f"num2 is largest")
+# else:
+#     print(f"{num3} is largest")
 
