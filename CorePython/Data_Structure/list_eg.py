ddcc09d84270c4588b8c8a2877b4f82a24eef73e
walkingacorn <akkusonglover15@gmail.com>
2026-05-12 10:32:11 +0530

    12th may classwork

---
+++ CorePython/Data_Structure/list_eg.py
@@ -0,0 +1,59 @@
+# lst = [1,2,3,4,2,'red','blue','green']
+# print(lst)
+# print(type(lst))
+# print(lst[5])
+# print(lst[-2])
+# print(lst[1:5])
+# print(lst[3:])
+# print(lst[:6])
+# print(lst[-6:-2])
+
+# print(len(lst))
+
+#concatenation, repetition, membership, length
+
+#picnic_items=['water','sandwich','umbrella','bag','clothes']
+
+# people=["Sam","Mary","Michael"]
+
+# picnic = picnic_items + people
+
+# print(picnic)
+
+# people1 = people*3
+# print(people1)
+
+# print("juice" in picnic_items)
+# print("juice" not in picnic_items)
+
+#List Methods - Adding Elements
+
+# picnic_items.append('fruit')
+# # print(picnic_items)
+# picnic_items.insert(2,'soda')
+# # print(picnic_items)
+# picnic_items.extend(['cake','icecream'])
+# print(picnic_items)
+
+#List Methods - Removing Elements
+
+# picnic_items.remove('sandwich')
+# print(picnic_items)
+# picnic_items.pop(2)
+# print(picnic_items)
+# picnic_items.clear()
+# print(picnic_items)
+
+
+#Sorting
+fruits=["banana","apple","cherry"]
+fruits.sort()
+print(fruits)
+
+numbers=[3,1,4,1,5]
+print(sorted(numbers)) #returns a new sorted list
+
+print(fruits.index("cherry"))
+print(numbers.count(1))
+fruits.reverse()
+print(fruits)
---
+++ CorePython/Data_Structure/task_list.py
@@ -0,0 +1,76 @@
+#1.Find the sum of all elements in a list
+# numbers=[1,2,3,4,5]
+# print(sum(numbers))
+
+
+
+#2. Find the average of list elements
+# average=sum(numbers)/len(numbers)
+# print(average)
+
+
+
+#3. Remove duplicate lements from a list
+# num=[1,2,1,3,4,5,5,2,6,7,8]
+# num2=set(num)
+# num3=list(num2)
+# print(num3)
+
+
+
+#4.Merge two lists into one
+# list1 = [1,2,3,4,5]
+# list2 =['apple','banana']
+# list3 =list1+list2
+# print(list3)
+
+
+
+#5. Find the second largest element in a list
+# num=[1,2,3,3,4,6,3,1,7,5,4,3,1,6,8,5,4]
+# num1=sorted(num)
+# print(num1[-2])
+
+#6. Find even and odd numbers from a list
+# num=[1,2,3,3,4,6,3,1,7,5,4,3,1,6,8,5,4]
+# even=[]
+# for item in num:
+#     if item%2==0:
+#         even.append(item)
+    
+# print(list(set(even)))
+
+
+
+#7. Create a new list containing squares of elements
+# num=[1,2,3,3,4,6,3,1,7,5,4,3,1,6,8,5,4]
+# squares=[]
+# for item in num:
+#     squares.append(item**2)
+    
+# print(squares)
+
+
+
+#8. Find common elements between two lists
+# list1=[1,2,3,4,5,6]
+# list2=[4,5,6,7,8,9]
+# list3=[]
+
+# for item in list1:
+#     if item in list2:
+#         list3.append(item)
+        
+# print(list3)
+
+
+
+#9. Find the index position of an element
+# list1=[1,2,3,4,5,6]
+# print(list1.index(4))
+
+
+
+#10. rotate a list to left by one position (output[2,3,4,5,1])
+# num=[1,2,3,4,5]
+# print(num[1:]+num[:1])
